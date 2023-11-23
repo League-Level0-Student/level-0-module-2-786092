5d5bd478a9ca34df4f3d1439bc02cd80212cba4e
IRFU <IRFU@DESKTOP-2MARTPS.attlocal.net>
2023-11-22 18:49:35 -0800

    Spot the Error is next

--- src/_03_string_conversion/_4_voting_booth/Voting_Booth.java
+++ src/_03_string_conversion/_4_voting_booth/Voting_Booth.java
@@ -1,5 +1,22 @@
 package _03_string_conversion._4_voting_booth;
 
-public class Voting_Booth {
+import javax.swing.JOptionPane;
 
+public class Voting_Booth {
+	public static void main(String[] agrs) {
+		String age=JOptionPane.showInputDialog("How old are you? ");
+		int vote = Integer.parseInt(age);
+		if(vote>=18) {
+			String president = JOptionPane.showInputDialog("Who should the next president be? ");
+			if(president.equals("Donald Trump")) {
+				JOptionPane.showMessageDialog(null, "Good Answer");
+			}
+			if(president.equals("Joe Biden")) {
+				JOptionPane.showMessageDialog(null, "Not a Good Answer");
+			}
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Nobody cares what you think");
+		}
+	}
 }
